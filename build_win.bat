ec32ab4acc827deaf889c988313c7175d4b7bd35
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-03-06 16:19:46 +0000

    Create build_win.bat

---
+++ build_win.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+mkdir build
+mkdir include
+mkdir src
+
+git clone https://github.com/axiomatic-systems/Bento4
+ 
+copy /y /v "Bento4\Source\C++\Core\*.h" "src"
+copy /y /v "Bento4\Source\C++\Core\*.cpp" "src"
+
+copy /y /v "Bento4\Source\C++\Codecs\*.h" "src"
+copy /y /v "Bento4\Source\C++\Codecs\*.cpp" "src"
+
+del Bento4
+
+REM Configure CMake
+cmake -B build -DCMAKE_BUILD_TYPE=Debug
+
+REM Build CMake
+cmake --build build --config Debug
+
+dir /s
+
+exit /b
