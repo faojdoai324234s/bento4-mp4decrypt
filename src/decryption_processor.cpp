ca045a528fdf2debf3f0d261c503dd07351981cc
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-03-06 22:34:45 +0000

    Create decryption_processor.cpp

---
+++ src/decryption_processor.cpp
@@ -0,0 +1,183 @@
+#include "DecryptionProcessor.h"
+
+
+/*----------------------------------------------------------------------
+|   DecryptionProcessor::DecryptionProcessor
++---------------------------------------------------------------------*/
+DecryptionProcessor::DecryptionProcessor() : processor(new AP4_CencDecryptingProcessor(nullptr)), keyMap(std::make_shared<AP4_ProtectionKeyMap>()), inputBuffer(new AP4_MemoryByteStream()) {
+	//
+}
+
+
+/*----------------------------------------------------------------------
+|   DecryptionProcessor::~DecryptionProcessor
++---------------------------------------------------------------------*/
+DecryptionProcessor::~DecryptionProcessor() {
+	
+	delete processor;
+	inputBuffer->Release();
+}
+
+
+/*----------------------------------------------------------------------
+|   DecryptionProcessor::decrypt
++---------------------------------------------------------------------*/
+bool DecryptionProcessor::decrypt(uint8_t** buffer, uintmax_t* length, const std::string& decryptionKey) {
+	
+	if (decryptionKey.size() != 32) {
+		return false;
+	}
+
+	// Create a key map object to hold decryption keys
+	unsigned char key[16];
+	AP4_ParseHex(decryptionKey, key, 16);
+	keyMap->SetKey(1, key, 16);
+
+	// Create the input stream
+	this->inputBuffer->SetBuffer(*buffer, *length);
+
+	// Create the output stream
+	AP4_MemoryByteStream* output = new AP4_MemoryByteStream();
+
+	// Set the decryption keys for the decrypting processor
+	processor->SetKeyMap(keyMap.get());
+
+	// Decrypt the file
+	AP4_Result result = processor->Process(*this->inputBuffer, *output);
+	this->inputBuffer->Clear();
+	if (AP4_FAILED(result)) {
+		output->Release();
+		return false;
+	}
+
+	// Prepare the original buffer
+	av_free(*buffer);
+	*length = output->GetDataSize();
+	*buffer = (uint8_t*)av_malloc(*length);
+
+	// Copy the decrypted data back to the original buffer
+	memcpy(*buffer, output->GetData(), *length);
+
+	// Clean up the data and return
+	output->Release();
+
+	return true;
+}
+
+
+/*----------------------------------------------------------------------
+|   DecryptionProcessor::decrypt
++---------------------------------------------------------------------*/
+bool DecryptionProcessor::decrypt(uint8_t** in_buffer, uintmax_t* in_length, uint8_t** out_buffer, uintmax_t* out_length, const std::string& decryptionKey) {
+
+	if (decryptionKey.size() != 32) {
+		return false;
+	}
+
+	// Create a key map object to hold decryption keys
+	unsigned char key[16];
+	AP4_ParseHex(decryptionKey, key, 16);
+	keyMap->SetKey(1, key, 16);
+
+	// Create the input stream
+	this->inputBuffer->SetBuffer(*in_buffer, *in_length);
+
+	// Create the output stream
+	AP4_MemoryByteStream* output = new AP4_MemoryByteStream();
+
+	// Set the decryption keys for the decrypting processor
+	processor->SetKeyMap(keyMap.get());
+
+	// Decrypt the file
+	AP4_Result result = processor->Process(*this->inputBuffer, *output);
+	this->inputBuffer->Clear();
+	if (AP4_FAILED(result)) {
+		output->Release();
+		return false;
+	}
+
+	// Prepare the output buffer
+	*out_length = output->GetDataSize();
+	*out_buffer = (uint8_t*)av_malloc(*out_length);
+
+	// Copy the decrypted data to the output buffer
+	memcpy(*out_buffer, output->GetData(), *out_length);
+
+	// Clean up the data and return
+	output->Release();
+
+	return true;
+}
+
+
+/*----------------------------------------------------------------------
+|   DecryptionProcessor::av_malloc
++---------------------------------------------------------------------*/
+void* DecryptionProcessor::av_malloc(size_t size) {
+	void* ptr = nullptr;
+
+	if (size > std::atomic_load_explicit(&max_alloc_size, std::memory_order_relaxed)) {
+		return nullptr;
+	}
+
+#if HAVE_POSIX_MEMALIGN
+	if (size) //OS X on SDK 10.6 has a broken posix_memalign implementation
+		if (posix_memalign(&ptr, ALIGN, size))
+			ptr = NULL;
+#elif HAVE_ALIGNED_MALLOC
+	ptr = _aligned_malloc(size, ALIGN);
+#elif HAVE_MEMALIGN
+#ifndef __DJGPP__
+	ptr = memalign(ALIGN, size);
+#else
+	ptr = memalign(size, ALIGN);
+#endif
+	/* Why 64?
+	 * Indeed, we should align it:
+	 *   on  4 for 386
+	 *   on 16 for 486
+	 *   on 32 for 586, PPro - K6-III
+	 *   on 64 for K7 (maybe for P3 too).
+	 * Because L1 and L2 caches are aligned on those values.
+	 * But I don't want to code such logic here!
+	 */
+	 /* Why 32?
+	  * For AVX ASM. SSE / NEON needs only 16.
+	  * Why not larger? Because I did not see a difference in benchmarks ...
+	  */
+	  /* benchmarks with P3
+	   * memalign(64) + 1          3071, 3051, 3032
+	   * memalign(64) + 2          3051, 3032, 3041
+	   * memalign(64) + 4          2911, 2896, 2915
+	   * memalign(64) + 8          2545, 2554, 2550
+	   * memalign(64) + 16         2543, 2572, 2563
+	   * memalign(64) + 32         2546, 2545, 2571
+	   * memalign(64) + 64         2570, 2533, 2558
+	   *
+	   * BTW, malloc seems to do 8-byte alignment by default here.
+	   */
+#else
+	ptr = malloc(size);
+#endif
+	if (!ptr && !size) {
+		size = 1;
+		ptr = av_malloc(1);
+	}
+#if CONFIG_MEMORY_POISONING
+	if (ptr)
+		memset(ptr, FF_MEMORY_POISON, size);
+#endif
+	return ptr;
+}
+
+
+/*----------------------------------------------------------------------
+|   DecryptionProcessor::av_free
++---------------------------------------------------------------------*/
+void DecryptionProcessor::av_free(void* ptr) {
+#if HAVE_ALIGNED_MALLOC
+	_aligned_free(ptr);
+#else
+	free(ptr);
+#endif
+}
