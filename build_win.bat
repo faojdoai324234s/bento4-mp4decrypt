03163490ec5fcf855e09181ec780208dca27a4ab
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-03-07 14:21:27 +0000

    Update build_win.bat

--- build_win.bat
+++ build_win.bat
@@ -5,6 +5,7 @@ mkdir build
 mkdir upload
 mkdir upload\include
 mkdir upload\Debug
+mkdir upload\Release
 
 git clone https://github.com/axiomatic-systems/Bento4
 
@@ -20,20 +21,25 @@ copy /y /v "Bento4\Source\C++\Crypto\*.cpp" src
 copy /y /v "Bento4\Source\C++\MetaData\*.h" src
 copy /y /v "Bento4\Source\C++\MetaData\*.cpp" src
 
-REM Configure CMake
+REM Configure CMake for Debug build
 cmake -B build -DCMAKE_BUILD_TYPE=Debug
 
-REM Build CMake
+REM Build CMake for Debug build
 cmake --build build --config Debug
 
-REM dir /s
-
+REM Copy over the built files
 copy /y /v build\Debug\*.dll upload\Debug
 copy /y /v build\Debug\*.lib upload\Debug
 copy /y /v build\Debug\*.pdb upload\Debug
 copy /y /v include upload\include
 
 rmdir /s /q build
+
+REM Configure CMake for Release build
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+
+REM Build CMake for Release build
+cmake --build build --config Release
 
 dir /s
 
