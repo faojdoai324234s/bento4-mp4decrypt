bff45017e8bc2ef54b3878b68467b73bd09ccec7
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-03-09 14:08:25 +0000

    Update build_linux.sh

--- build_linux.sh
+++ build_linux.sh
@@ -9,26 +9,25 @@ mkdir upload/Debug
 mkdir upload/Release
 
 git clone https://github.com/axiomatic-systems/Bento4
-ls
-ls upload/
+
 # Copy the source files which will be overwritten
-cp src\*.cpp temp
+cp src/*.cpp temp/
 
 # Copy over the source files from Bento4
-cp "Bento4\Source\C++\Core\*.h" src
-cp "Bento4\Source\C++\Core\*.cpp" src
+cp "Bento4/Source/C++/Core/*.h" src/
+cp "Bento4/Source/C++/Core/*.cpp" src/
 
-cp "Bento4\Source\C++\Codecs\*.h" src
-cp "Bento4\Source\C++\Codecs\*.cpp" src
+cp "Bento4/Source/C++/Codecs/*.h" src/
+cp "Bento4/Source/C++/Codecs/*.cpp" src/
 
-cp "Bento4\Source\C++\Crypto\*.h" src
-cp "Bento4\Source\C++\Crypto\*.cpp" src
+cp "Bento4/Source/C++/Crypto/*.h" src/
+cp "Bento4/Source/C++/Crypto/*.cpp" src/
 
 # Copy back the files which were overwritten
-cp temp\*.cpp src
+cp temp/*.cpp src/
 
-cp "Bento4\Source\C++\MetaData\*.h" src
-cp "Bento4\Source\C++\MetaData\*.cpp" src
+cp "Bento4/Source/C++/MetaData/*.h" src/
+cp "Bento4/Source/C++/MetaData/*.cpp" src/
 
 # Configure CMake for Debug build
 cmake -B build -DCMAKE_BUILD_TYPE=Debug
@@ -37,12 +36,12 @@ cmake -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build --config Debug
 
 # Copy over the built files
-# cp build\Debug\*.dll upload\Debug
-# cp build\Debug\mp4decrypt.lib upload\Debug\mp4decrypt_debug.lib
-# cp build\Debug\*.pdb upload\Debug
+# cp build/Debug/*.dll upload/Debug/
+# cp build/Debug/mp4decrypt.lib upload/Debug/mp4decrypt_debug.lib
+# cp build/Debug/*.pdb upload/Debug/
 
 # Clean up before we run CMake again
-rmdir /s /q build
+rmdir /s /q build/
 
 # Configure CMake for Release build
 cmake -B build -DCMAKE_BUILD_TYPE=Release
@@ -51,8 +50,8 @@ cmake -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
 
 # Copy over the built files
-# cp build\Release\*.dll upload\Release
-# cp build\Release\*.lib upload\Release
+# cp build/Release/*.dll upload/Release/
+# cp build/Release/*.lib upload/Release/
 
 # Copy over the headers
-cp include upload\include\mp4decrypt
+cp include/ upload/include/mp4decrypt/
